ce848ce30c115b49d952b0f4b32d9d54c976eba2
Vikrant Mantalwad <vmantalwad@gmail.com>
2025-03-28 11:40:27 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Footer from "./components/Footer.js";
+import About from "./components/About.js";
+import Hero from "./components/Hero.js";
+import Services from "./components/Services.js";
+import Tours from "./components/Tours.js";
+import Navbarr from "./components/Navbarr.js";
+
+function App() {
+  return (
+    <>
+      <Navbarr />
+      <Hero />
+      <About />
+      <Services />
+      <Tours />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
